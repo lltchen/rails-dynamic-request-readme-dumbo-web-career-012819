fca83966c70e0306279a9a913c4d3b792e171a90
Elliot Chen <lltchen1@gmail.com>
2019-02-25 19:57:11 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 Post.create(title:"Your Mother", description:"safsafdfasdfsdaf")
 Post.create(title:"Your Sister", description:"safsafdfasdfsdaf")
 Post.create(title:"Your Father", description:"safsafdfasdfsdaf")
-Post.create(title:"Your Brother", description:"cat")
+Post.create(title:"Your Mother", description:"safsafdfasdfsdaf")

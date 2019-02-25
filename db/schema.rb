6c61d422f38c2d786c2c35935dd2661c37c4c5c8
Elliot Chen <lltchen1@gmail.com>
2019-02-25 20:03:06 +0000

    Done.

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+
+
+ActiveRecord::Schema.define(version: 20151230232412) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title:"Your Mother", description:"safsafdfasdfsdaf")
+Post.create(title:"Your Sister", description:"safsafdfasdfsdaf")
+Post.create(title:"Your Father", description:"safsafdfasdfsdaf")
+Post.create(title:"Your Mother", description:"safsafdfasdfsdaf")
